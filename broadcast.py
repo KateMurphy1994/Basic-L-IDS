89cc187c8e2f5df9bb28f588956d813fa6291810
KateMurphy1994 <134777363+KateMurphy1994@users.noreply.github.com>
2023-05-27 13:32:14 +0300

    Update broadcast.py

--- broadcast.py
+++ broadcast.py
@@ -6,7 +6,6 @@ class Broadcast:
     def sendmail(self, message):
         if not message:
             return
-        command = f"echo '" + message + "'"
-        print(command)
-        os.system(command)# Mail alert (Mail reducted; used to use the mail command with a local smtp server)
+        command = f"echo '" + message + "' | mail -s 'INTRUSION ALERT!' -a 'FROM: IDS@security.htb' kate.murphy@htb.com"
+        os.system(command)# Mail alert 
         os.system('mv /var/log/IDS/alert.log /var/log/IDS/alert.log.{0}.{1}.{2}'.format(datetime.datetime.now().day, datetime.datetime.now().hour, datetime.datetime.now().minute))
